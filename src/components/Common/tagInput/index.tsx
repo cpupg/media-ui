1ad933f3a416fa24067c01619097d18b595d90a0
chen <39870481+cpupg@users.noreply.github.com>
2024-12-12 23:06:04 +0800

    优化TagInput，可以从父组件传入taglist

--- src/components/Common/tagInput/index.tsx
+++ src/components/Common/tagInput/index.tsx
@@ -16,9 +16,22 @@ interface PropsType {
    */
   onClose?: (value: string) => void;
   /**
+   * 输入框改变时的回调。
+   * @param value 当前输入框的值
+   */
+  onValueChange?: (value: string) => void;
+  /**
    * 是否可以删除标签。
    */
   close?: boolean;
+  /**
+   * 标签中的内容。
+   */
+  valueList?: string[];
+  /**
+   * 当前输入的内容。
+   */
+  value?: string;
 }
 
 /**
@@ -27,7 +40,7 @@ interface PropsType {
  * @returns 标签输入框。
  */
 const TagInput: React.FC<PropsType> = (props) => {
-  const { onEnter, onClose, close } = props;
+  const { onEnter, onClose, close, onValueChange, valueList, value } = props;
 
   const [tagList, setTagList] = useState<string[]>([]);
   const [current, setCurrent] = useState('');
@@ -53,21 +66,40 @@ const TagInput: React.FC<PropsType> = (props) => {
     }
   };
 
+  const onChange = (current: string) => {
+    setCurrent(current);
+    if (onValueChange) {
+      onValueChange(current);
+    }
+  };
+
+  const renderTag = () => {
+    if (valueList) {
+      return valueList.map((v) => (
+        <Tag onClose={(e) => handleClose(v)} closable={close} key={v}>
+          {v}
+        </Tag>
+      ));
+    } else {
+      return tagList.map((v) => (
+        <Tag onClose={(e) => handleClose(v)} closable={close} key={v}>
+          {v}
+        </Tag>
+      ));
+    }
+  };
+
   return (
     // 设置最小高度，否则没有标签时div没有高度。
     // todo 使用span展示输入内容，隐藏input，鼠标点击div时聚焦
     <div className="ant-input" style={{ minHeight: 0 }}>
-      {tagList.map((v) => (
-        <Tag onClose={(e) => handleClose(v)} closable={close} key={v}>
-          {v}
-        </Tag>
-      ))}
+      {renderTag()}
       <Input
         onPressEnter={(e) => handleEnter(current)}
         bordered={false}
         // style={{ width: 50 }}
         value={current}
-        onChange={(e) => setCurrent(e.target.value)}
+        onChange={(e) => onChange(e.target.value)}
       />
       {/* <span>{current}</span> */}
     </div>
