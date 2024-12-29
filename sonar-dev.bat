dc02fdbd336630ff2935d26a3ddaa46d60a3ed8e
chen <39870481+cpupg@users.noreply.github.com>
2024-12-29 22:17:43 +0800

    增加sonar扫描脚本

---
+++ sonar-dev.bat
@@ -0,0 +1,1 @@
+sonar-scanner -Dsonar.projectKey=media-ui-dev -Dsonar.projectName='media-ui-dev' -Dsonar.host.url=http://sonarqube.local.net:9000 -Dsonar.token=%1
---
+++ sonar-feature.bat
@@ -0,0 +1,1 @@
+sonar-scanner -Dsonar.projectKey=media-ui-feature -Dsonar.projectName='media-ui-feature' -Dsonar.host.url=http://sonarqube.local.net:9000 -Dsonar.token=%1
